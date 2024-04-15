8c98ad8021c956362c126fa9295a72547c9029bc
Mohamed Samir <121590212+llMOllSamir@users.noreply.github.com>
2024-04-15 23:04:59 +0200

    create Routes

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="fixed inset-0 bg-red-600 h-16"></header>
+        {children}
+      </body>
     </html>
   );
 }
---
+++ app/products/product-detailes/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ProductDetailes() {
+    return (
+        <div className='mt-20'>ProductDetailes</div>
+    )
+}
